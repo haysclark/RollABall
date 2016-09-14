e7e085380e90bcc848b69df54b33467bfe2e4a48
Hays Clark <hays.clark@gmail.com>
2016-09-13 22:02:34 -0700

    Adding project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after Update is complete
+	void LateUpdate () {
+		transform.position = target.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start () {
+		count = 0;
+		rb = GetComponent<Rigidbody> ();
+		winText.text = "";
+		updateCountText ();
+	}
+
+	// FixedUpdate is called once per frame before any physics calcs.
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical) * speed;
+		rb.AddForce (movement);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count++;
+			updateCountText ();
+			checkForWin ();
+		}
+	}
+
+	private void checkForWin () {
+		if (count < 12) {
+			return;
+		}
+
+		showWinText ();
+	}
+
+	private void showWinText () {
+		winText.text = "You Win!";
+	}
+
+	private void updateCountText () {
+		countText.text = "Count: " + count.ToString ();
+	}
+}
